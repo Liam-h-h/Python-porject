60f41c6fcb28a60bd55b7a52a227fb3524ec2fbb
Liam-h-h <liamharrington1125@gmail.com>
2021-02-22 22:45:12 +1300

    initial commit

--- main.py
+++ main.py
@@ -13,47 +13,27 @@ import arcade
 arcade.open_window(600, 600, "Drawing Example")
 
 # Set the background color
-arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+arcade.set_background_color(arcade.color.SUNSET_ORANGE)
 
 # Get ready to draw
 arcade.start_render()
 
+# Draw Sun
+arcade.draw_arc_filled(300, 340, 180, 180, arcade.color.YELLOW_ORANGE, -180, 180)
+
 # Draw a rectangle
 # Left of 0, right of 599
 # Top of 300, bottom of 0
-arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.BLUE)
+arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.color.YALE_BLUE)
 
-# Draw waves
-arcade.draw_arc_filled(300, 280, 60, 100, arcade.csscolor.BLUE, 0, 180)
-arcade.draw_arc_filled(390, 280, 60, 100, arcade.csscolor.BLUE, 0, 180)
-arcade.draw_arc_filled(480, 280, 60, 100, arcade.csscolor.BLUE, 0, 180)
-arcade.draw_arc_filled(210, 280, 60, 100, arcade.csscolor.BLUE, 0, 180)
-arcade.draw_arc_filled(110, 280, 60, 100, arcade.csscolor.BLUE, 0, 180)
 
-# Draw a boat
 
-arcade.draw_line(300, 320, 300, 450, arcade.color.SIENNA, 6)
-arcade.draw_rectangle_filled(300, 320, 200, 60, arcade.csscolor.SIENNA)
-arcade.draw_arc_filled(300, 400, 60, 100, arcade.csscolor.WHITE_SMOKE, 0, 180)
+# Draw a Boat
+arcade.draw_arc_filled(250, 250, 155, 60, arcade.csscolor.DARK_RED, 180, 360)
+arcade.draw_rectangle_filled(253, 300, 6, 100, arcade.csscolor.BLACK)
+arcade.draw_triangle_filled(250,250,180,250,250,350, arcade.csscolor.WHITE)
+arcade.draw_triangle_filled(255,250,320,250,255,350, arcade.csscolor.WHITE)
 
-# Draw a sun
-arcade.draw_circle_filled(500, 550, 40, arcade.color.YELLOW)
-
-# Rays to the left, right, up, and down
-arcade.draw_line(500, 550, 400, 550, arcade.color.YELLOW, 3)
-arcade.draw_line(500, 550, 600, 550, arcade.color.YELLOW, 3)
-arcade.draw_line(500, 550, 500, 450, arcade.color.YELLOW, 3)
-arcade.draw_line(500, 550, 500, 650, arcade.color.YELLOW, 3)
-
-# Diagonal rays
-arcade.draw_line(500, 550, 550, 600, arcade.color.YELLOW, 3)
-arcade.draw_line(500, 550, 550, 500, arcade.color.YELLOW, 3)
-arcade.draw_line(500, 550, 450, 600, arcade.color.YELLOW, 3)
-arcade.draw_line(500, 550, 450, 500, arcade.color.YELLOW, 3)
-
-# Draw Fish
-arcade.draw_ellipse_filled(200, 150, 350, 200, arcade.csscolor.DARK_GREEN)
-arcade.draw_arc_filled(300, 340, 150, 350, arcade.csscolor.DARK_GREEN, 90, 180)
 
 
 #Finish Drawing
